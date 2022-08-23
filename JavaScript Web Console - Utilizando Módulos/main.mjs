7cfc535f0e56d580fc5c21730e53df259b6e890d
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-22 21:26:43 -0300

    JavaScript Web Console - Utilizando Módulos em JS V1

---
+++ JavaScript Web Console - Utilizando Módulos/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade('Luciano', 31));
+console.log(mostraCidade('Luciano', 'Campos dos Goytacazes'));
+console.log(mostraHobby('Dayana', 'Estudar e ler livros'));
